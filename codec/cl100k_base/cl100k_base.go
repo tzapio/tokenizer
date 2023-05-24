5ffec6cc6e5c11115f60a871018bdc7310c19c4e
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-05-24 23:26:55 +0000

    switching to embedded file

--- codec/cl100k_base/cl100k_base.go
+++ codec/cl100k_base/cl100k_base.go
@@ -1,11 +1,20 @@
 package cl100k_base
 
 import (
+	"bufio"
+	"bytes"
+	"embed"
+	"encoding/base64"
+	"log"
+	"strconv"
+	"strings"
+
 	"github.com/dlclark/regexp2"
 	"github.com/tzapio/tokenizer/codec"
 )
 
 func NewCl100kBase() *codec.Codec {
+	cl100kBaseVocab := loadVocab("cl100k_base.tiktoken")
 	return codec.New(
 		"cl100k_base",
 		cl100kBaseVocab,
@@ -18,4 +27,39 @@ func NewCl100kBase() *codec.Codec {
 			"<|endofprompt|>": 100276,
 		})
 
+}
+
+//go:embed cl100k_base.tiktoken
+var vocabData embed.FS
+
+func loadVocab(filename string) codec.Vocab {
+	var vocab codec.Vocab = make(map[string]uint)
+	data, err := vocabData.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("failed to read vocab file: %s", err)
+	}
+	reader := bytes.NewReader(data)
+	scanner := bufio.NewScanner(reader)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, " ")
+
+		if len(parts) != 2 {
+			log.Fatalf("invalid line: %s", line)
+		}
+
+		word, err := base64.StdEncoding.DecodeString(parts[0])
+		if err != nil {
+			log.Fatalf("invalid word: %s", parts[0])
+		}
+
+		v, err := strconv.ParseUint(parts[1], 10, 64)
+		if err != nil {
+			panic("invalid word: " + parts[1])
+		}
+		vocab[string(word)] = uint(v)
+	}
+
+	return vocab
 }
